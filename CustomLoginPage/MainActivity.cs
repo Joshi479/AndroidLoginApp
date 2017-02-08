0424a742a46ded3975074acd66148aa639e2e994
Joshi479 <joshisaiteja79@gmail.com>
2017-02-08 15:15:12 -0800

    Add project files.

---
+++ CustomLoginPage/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace CustomLoginPage
+{
+    [Activity(Label = "CustomLoginPage", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
